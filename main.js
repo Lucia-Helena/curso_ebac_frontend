0565cfffb7e715769bafa40cb00c92bac0cd9cd3
Lúcia Helena <luciahproenca@gmail.com>
2023-10-28 21:52:51 -0200

    Fiz modificações no código

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-const form = document.getElementById('form-number');
+const form = document.getElementById('form');
+const number1 = document.getElementById('Input1');
+const number2 = document.getElementById('Input2');
+const reserved = document.getElementById('reserved');
+const error = document.getElementById('error');
 
-function validaNumero() {
-    
+
+function number(number1, number2) {
+    return number1 < number2
 }
 
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-console.log('form');
+    let validate1 = number(number1.valueAsNumber, number2.valueAsNumber)
+    if(validate1) {
+        reserved.style.display = 'block';
+        number2.value=''
+        number1.value=''
+    }
+
+
+})
+
+number1.addEventListener('keyup', function(e){
+
+    let validate1 = number(number1.valueAsNumber, number2.valueAsNumber)
+    if(!validate1){
+       error.style.display = 'block';
+       reserved.style.display = 'none';
+
+    } else {
+        error.style.display = 'none';
+    }
+})
+
+
