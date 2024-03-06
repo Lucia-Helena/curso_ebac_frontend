ff2b8dd4d56254b4f18f6e66ca13e3e51470a6f5
Lúcia Helena <luciahproenca@gmail.com>
2024-03-06 19:19:00 -0300

    Fiz modificações no código

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 
+gulp.task('sass', function() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('sass', 'watch'))
+
 function compressImages() {
-  return gulp.src('src/images/*')
+  return gulp.src('src/scss/images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'));
 }
